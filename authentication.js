bf33e17df509a0014a920f68db17f3208058cb5b
Veikko Salminen <veikko.salminen@visma.com>
2020-07-27 11:48:46 +0300

    scope support

--- authentication.js
+++ authentication.js
@@ -14,13 +14,15 @@ class authentication {
     async login(queryParams) {
         const body = tools.parseQuery(queryParams)
         
-        let user
+        let user, scopes
 
         if (body.grant_type === "refresh_token") {
             const claims = jwt.verify(body.refresh_token, this.params.public_key)
 
             if (claims.tokentype && claims.tokentype !== "refresh") throw new Error("Cannot refresh with token type: " + claims.tokentype)
         
+            scopes = claims.scope
+
             // TODO authorizer should be verified also .. not just username .. If username is changed or something
             const users = await this.api.query("users", {filter: {"attributes.username": claims.username}})
 
@@ -28,6 +30,8 @@ class authentication {
             user = users[0]
         }
         else {
+            scopes = body.scope
+
             const users = await this.api.query("users", {filter: {"attributes.username": body.username}})
 
             if (users.length < 1) return {statusCode: 400, body: JSON.stringify({error: "login failed"})}
@@ -37,24 +41,34 @@ class authentication {
             if (!match) return {statusCode: 400, body: JSON.stringify({error: "login failed"})}
         }
 
-        const obj = {
+        const tokenObj = {
             client_id: body.client_id
         }
 
         for (const key in this.params.values) {
             const value = tools.get(user, this.params.values[key])
             if (!value) throw new Error("Authorizer object creation failed because '" + this.params.values[key] + "' is not set in user data")
-            obj[key] = value
+            tokenObj[key] = value
         }
 
         await this.api.patch("users", user.id, {"attributes.lastlogin": new Date()})
 
+        if (scopes) {
+            const userScopes = await this.api.query("userscopes", {filter: {
+                "attributes.scope": {'$in': scopes.split(" ")},
+                "relationships.user.data.id": user.id
+            }})
+            tokenObj.scope = userScopes.map((item) => item.attributes.scope).join(" ")
+        }
+
         const response = {
-            access_token: jwt.sign(obj, this.privateKey, { algorithm: 'RS256', expiresIn: this.params.expires}),
-            refresh_token: jwt.sign(Object.assign(obj, {type: "refresh"}), this.privateKey, { algorithm: 'RS256', expiresIn: this.params.refresh_expires || 3600 * 24 * 14}),
+            access_token: jwt.sign(tokenObj, this.privateKey, { algorithm: 'RS256', expiresIn: this.params.expires}),
+            refresh_token: jwt.sign(Object.assign(tokenObj, {type: "refresh"}), this.privateKey, { algorithm: 'RS256', expiresIn: this.params.refresh_expires || 3600 * 24 * 14}),
             expires_in: this.params.expires,
             token_type: "Bearer"
         }
+
+        if (scopes) response.scope = tokenObj.scope
 
         if (this.params.includeUserDocument) response.user = user
        
